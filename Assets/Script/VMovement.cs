141440555fedb8928f9bbbbd7c7613bebdc9956e
HaydenCreates <125945199+HaydenCreates@users.noreply.github.com>
2024-02-15 08:59:34 -0600

    Add files via upload

--- Assets/Script/VMovement.cs
+++ Assets/Script/VMovement.cs
@@ -7,12 +7,17 @@ using Cinemachine;
 public class VMovement : MonoBehaviour
 {
     private Rigidbody cube_RigidBody;
-    public float speed = 3.0f;
+    public float baseSpeed = 3.0f;
+    public float sprintSpeed = 6.0f;
+    public float currentSpeed;
+
+    public float sprintDuration = 3.0f;  // Adjust the duration as needed
+    private float sprintTimer;
 
     private PlayerInput playerInput;
     private PlayerControls playerControl;
-    private CinemachineFreeLook freeLookCamera;
     public bool isSprint;
+    Vector3 movement;
 
     //subscribing to actions mean you can use them?
 
@@ -21,18 +26,53 @@ public class VMovement : MonoBehaviour
         if (context.performed)
         {
             Debug.Log("Sprint Started");
-            isSprint = true;
-            speed = 6.0f;
+            StartSprint();
 
         }
 
         if (context.canceled)
         {
             Debug.Log("Sprint Stopped");
-            isSprint = false;
-            speed = 3.0f;
+            StopSprint();
         }
     }
+
+    private void StartSprint()
+    {
+        if (!isSprint)
+        {
+            Debug.Log("Sprint Started");
+            isSprint = true;
+            currentSpeed = sprintSpeed;
+            StartCoroutine(SprintTimer());
+        }
+    }
+
+    private void StopSprint()
+    {
+        if (isSprint)
+        {
+            Debug.Log("Sprint Stopped");
+            isSprint = false;
+            currentSpeed = baseSpeed;
+        }
+    }
+
+    private IEnumerator SprintTimer()
+    {
+        sprintTimer = sprintDuration;
+
+        while (sprintTimer > 0 && isSprint)
+        {
+            yield return new WaitForSeconds(1.0f);
+            sprintTimer--;
+
+            // Optionally, you can update UI or perform other actions based on remaining sprint time
+        }
+
+        StopSprint();
+    }
+
 
     private void OnMove(InputAction.CallbackContext context)
     {
@@ -46,21 +86,25 @@ public class VMovement : MonoBehaviour
 
         if (mainCamera != null)
         {
+
             // Transform input to world space based on camera's forward and right vectors
             Vector3 forward = mainCamera.transform.forward.normalized;
             Vector3 right = mainCamera.transform.right.normalized;
 
-            Vector3 movement = (forward * movementInput.y + right * movementInput.x).normalized * speed;
-
-            // Apply movement to the character
-            cube_RigidBody.velocity = new Vector3(movement.x, cube_RigidBody.velocity.y, movement.z);
-            cube_RigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            movement = (forward * movementInput.y + right * movementInput.x).normalized * currentSpeed;
 
         }
         else
         {
             Debug.LogError("Main Camera not found.");
         }
+    }
+
+    //allows for continous movement
+    private void FixedUpdate()
+    {
+        cube_RigidBody.velocity = new Vector3(movement.x, cube_RigidBody.velocity.y, movement.z);
+        cube_RigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     private void OnEnable()
@@ -70,10 +114,10 @@ public class VMovement : MonoBehaviour
             playerControl = new PlayerControls();
             playerControl.Enable();
             playerInput = GetComponent<PlayerInput>();
-            playerInput.actions["Sprint"].performed += OnSprint;
-            playerInput.actions["Sprint"].started += OnSprint;
             playerInput.actions["Move"].performed += OnMove;
             playerInput.actions["Move"].started += OnMove;
+            playerInput.actions["Sprint"].performed += OnSprint;
+            playerInput.actions["Sprint"].started += OnSprint;
         }
     }
 
@@ -82,12 +126,13 @@ public class VMovement : MonoBehaviour
         if (playerControl != null)
         {
             playerControl.Disable();
-            playerInput.actions["Sprint"].performed -= OnSprint;
-            playerInput.actions["Sprint"].canceled -= OnSprint;
-            playerInput.actions["Sprint"].started -= OnSprint;
             playerInput.actions["Move"].started -= OnMove;
             playerInput.actions["Move"].performed -= OnMove;
             playerInput.actions["Move"].canceled -= OnMove;
+            playerInput.actions["Sprint"].performed -= OnSprint;
+            playerInput.actions["Sprint"].canceled -= OnSprint;
+            playerInput.actions["Sprint"].started -= OnSprint;
+            
         }
     }
 
@@ -95,18 +140,20 @@ public class VMovement : MonoBehaviour
     {
         playerControl = new PlayerControls();
         playerInput = GetComponent<PlayerInput>();
-        playerInput.actions["Sprint"].performed += OnSprint;
-        playerInput.actions["Sprint"].canceled += OnSprint;
-        playerInput.actions["Sprint"].started += OnSprint;
         playerInput.actions["Move"].performed += OnMove;
         playerInput.actions["Move"].canceled += OnMove;
         playerInput.actions["Move"].started += OnMove;
+        playerInput.actions["Sprint"].performed += OnSprint;
+        playerInput.actions["Sprint"].canceled += OnSprint;
+        playerInput.actions["Sprint"].started += OnSprint;
+        
 
     }
 
     void Start()
     {
         cube_RigidBody = GetComponent<Rigidbody>();
+        currentSpeed = baseSpeed;
     }
 
    
